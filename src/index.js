34431203fa4f8cdba16913417cfea7b7286b3ca1
Damian Arechar <damian.lon.are@gmail.com>
2017-11-01 09:07:34 -0600

    Loading Images and Fonts with Webpack.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
   var element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
