f6dfdc1623762bac78338f5b79363ce2859d4660
Damian Arechar <damian.lon.are@gmail.com>
2017-11-01 09:22:27 -0600

    Deleting alot of files because the tutorial said that isn't necessary in the next chapter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  var myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 }
