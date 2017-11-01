80651d7ac7dab6e0ffe4ad7f8306694124233e94
Damian Arechar <damian.lon.are@gmail.com>
2017-11-01 16:15:53 -0600

    Finishing the basic and intermediate tutorials of Webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-import _ from 'lodash';
+//import _ from 'lodash';
+import { cube } from './math.js';
+import printMe from './print.js';
+import './style.css';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 
 function component() {
-  var element = document.createElement('div');
+  var element = document.createElement('pre');
+  var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['hello', 'webpack'], ' ');
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' /*+ cube(5)*/
+  ].join('\n\n');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // Store the element ro re-render on print.js changes
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    document.bodu.appendChild(element);
+  })
+}
---
+++ webpack.prod.js
@@ -0,0 +1,17 @@
+const merge = require('webpack-merge');
+const UglifyPlugin = require('uglifyjs-webpack-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'source-map',
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
+})
