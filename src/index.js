a9d9e79f0caf5e85c1c77d310b353a487a59f514
Damian Arechar <damian.lon.are@gmail.com>
2017-11-01 08:31:51 -0600

    Loading CSS with css-loader and style-loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
